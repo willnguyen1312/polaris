09a3d3f02b8e8a1c771f5462e7916e71045b419e
Marc Thomas <marc.thomas@shopify.com>
2023-10-02 10:45:42 +0100

    chore: react to changes in dynamic filters

--- polaris-react/src/components/IndexFilters/IndexFilters.stories.tsx
+++ polaris-react/src/components/IndexFilters/IndexFilters.stories.tsx
@@ -981,6 +981,7 @@ export function WithAsyncData() {
   const sleep = (ms: number) =>
     new Promise((resolve) => setTimeout(resolve, ms));
   const [loadData, setLoadData] = useState(false);
+  const [addAsyncFilter, setAddAsyncFilter] = useState(false);
   const [itemStrings, setItemStrings] = useState([
     'All',
     'Unpaid',
@@ -1067,7 +1068,7 @@ export function WithAsyncData() {
     {label: 'Total', value: 'total desc', directionLabel: 'Descending'},
   ];
   const [sortSelected, setSortSelected] = useState(['order asc']);
-  const {mode, setMode} = useSetIndexFiltersMode();
+  const {mode, setMode} = useSetIndexFiltersMode(IndexFiltersMode.Filtering);
   const onHandleCancel = () => {};
 
   const onHandleSave = async () => {
@@ -1094,6 +1095,10 @@ export function WithAsyncData() {
   ]);
   const [moneySpent, setMoneySpent] = useState(null);
   const [taggedWith, setTaggedWith] = useState('Returning customer');
+  const [deliveryMethod, setDeliveryMethod] = useState<string[] | null>([
+    'local_pick_up',
+    'local_delivery',
+  ]);
   const [queryValue, setQueryValue] = useState('');
 
   const handleAccountStatusChange = useCallback(
@@ -1108,6 +1113,10 @@ export function WithAsyncData() {
     (value) => setTaggedWith(value),
     [],
   );
+  const handleDeliveryMethodChange = useCallback(
+    (value) => setDeliveryMethod(value),
+    [],
+  );
   const handleFiltersQueryChange = useCallback(
     (value) => setQueryValue(value),
     [],
@@ -1118,17 +1127,23 @@ export function WithAsyncData() {
   );
   const handleMoneySpentRemove = useCallback(() => setMoneySpent(null), []);
   const handleTaggedWithRemove = useCallback(() => setTaggedWith(''), []);
+  const handleDeliveryMethodRemove = useCallback(
+    () => setDeliveryMethod(null),
+    [],
+  );
   const handleQueryValueRemove = useCallback(() => setQueryValue(''), []);
   const handleFiltersClearAll = useCallback(() => {
     handleAccountStatusRemove();
     handleMoneySpentRemove();
     handleTaggedWithRemove();
+    handleDeliveryMethodRemove();
     handleQueryValueRemove();
   }, [
     handleAccountStatusRemove,
     handleMoneySpentRemove,
     handleQueryValueRemove,
     handleTaggedWithRemove,
+    handleDeliveryMethodRemove,
   ]);
 
   const filters = [
@@ -1185,6 +1200,28 @@ export function WithAsyncData() {
     },
   ];
 
+  if (addAsyncFilter) {
+    filters.push({
+      key: 'delivery_method',
+      label: 'Delivery method',
+      filter: (
+        <ChoiceList
+          title="Delivery method"
+          titleHidden
+          choices={[
+            {label: 'Local pick up', value: 'local_pick_up'},
+            {label: 'Local delivery', value: 'local_delivery'},
+            {label: 'National delivery', value: 'national_delivery'},
+            {label: 'International delivery', value: 'international_delivery'},
+          ]}
+          selected={deliveryMethod || []}
+          onChange={handleDeliveryMethodChange}
+          allowMultiple
+        />
+      ),
+    });
+  }
+
   const appliedFilters: IndexFiltersProps['appliedFilters'] = [];
   if (!isEmpty(accountStatus)) {
     const key = 'accountStatus';
@@ -1210,6 +1247,16 @@ export function WithAsyncData() {
       onRemove: handleTaggedWithRemove,
     });
   }
+  if (!isEmpty(deliveryMethod)) {
+    const key = 'delivery_method';
+    appliedFilters.push({
+      key,
+      label: disambiguateLabel(key, deliveryMethod),
+      onRemove: handleDeliveryMethodRemove,
+    });
+  }
+
+  console.log({addAsyncFilter, filters});
 
   return (
     <Card padding="0">
@@ -1243,6 +1290,9 @@ export function WithAsyncData() {
         <Button size="micro" primary onClick={() => setLoadData(true)}>
           Load filter data
         </Button>
+        <Button size="micro" primary onClick={() => setAddAsyncFilter(true)}>
+          Add async filter
+        </Button>
       </div>
     </Card>
   );
@@ -1255,6 +1305,8 @@ export function WithAsyncData() {
         return `Tagged with ${value}`;
       case 'accountStatus':
         return value.map((val) => `Customer ${val}`).join(', ');
+      case 'delivery_method':
+        return `Delivery method: ${value.join(', ')}`;
       default:
         return value;
     }
